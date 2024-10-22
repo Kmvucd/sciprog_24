e0659e3e82c709a816492e2d290ce2da284220e9
Vimarish K M <vimarish.kuttapalayammani@ucdconnect.ie>
2024-10-22 15:18:39 +0100

    Added comments

--- practical5/hyp.c
+++ practical5/hyp.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <math.h>
-
+// function declaration
 double artanh1(double x, double delta);
 double artanh2(double x);
 int compare(double p, double q);
@@ -22,10 +22,6 @@ for (int i = 0; i<=180; i++)
 {
 b = -0.9 + (i * 0.01);
 mc[i] = artanh1(b,a);
-//printf("%f",mc[i]);
-nl[i] = artanh2(b);
-//printf("%f",nl[i]);
-
 printf("%s\n",compare(mc[i],nl[i]) ? "Yes":"No");
 }
 }
@@ -42,17 +38,16 @@ term = (pow(x, 2 * i + 1)/(2 * i + 1));
 result += term;
 i++;
 }
-//printf("%f",out);
 return result;
 }
-
+// function definition to find values from natural algorithm
 double artanh2(double x)
 {
 double a = 0.5, res;
 res = a *(log(1+x)-log(1-x)); 
 return res;
 }
-
+// function definition to compare values from two comparitives
 int compare(double p, double q)
 {
 p = round(p * 1e10)/1e10;
