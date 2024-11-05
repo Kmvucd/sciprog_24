ca30c374de7317f7bb07567c21827bfb1342bd56
Kmvucd <vimarish.kuttapalayammani@ucdconnect.ie>
2024-11-05 20:29:28 +0000

    Update gcd_rec.c

--- practical8/gcd_rec.c
+++ practical8/gcd_rec.c
@@ -1,5 +1,5 @@
 # include <stdio.h>
-
+// Function to calculate gcd using recursive approach
 int gcd(int a,int b)
 {
 if (b == 0)
@@ -15,12 +15,12 @@ return gcd(b, a%b);
 int main()
 {
 int a, b;
-printf("Enter value of a:");
+printf("Enter value of a:");//Declaring a variable
 scanf("%d",&a);
-printf("Enter value of b:");
+printf("Enter value of b:"); //Declaring b variable
 scanf("%d",&b);
 int result ; 
-result = gcd(a,b);
+result = gcd(a,b); // calling gcd function
 printf("GCD is %d",result);
 
 }
