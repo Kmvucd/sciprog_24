f00224048f7fc0e107013aff8925f58049a4773f
Vimarish K M <vimarish.kuttapalayammani@ucdconnect.ie>
2024-11-23 23:52:05 +0000

    Added practical5

---
+++ practical5/fib.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+// Function declaration with input as pointer
+void fib(int *x, int *y);
+
+int main()
+{
+int a, f0 = 0, f1 = 1;
+// Enter value of n
+printf("Enter the value: ");
+scanf("%d",&a);
+//printing first 2 values
+if (a == 0)
+{
+printf("Fibonacci series is %d",f0);
+}
+else 
+{
+printf("Fibonacci series is %d %d ",f0,f1);
+}
+// Looping to pass values till a
+for (int i = 2; i<=a; i++)
+{
+fib(&f0,&f1);
+}
+return 0;
+}
+
+// function to generate fibonacci series
+void fib(int *x, int *y)
+{
+int temp = *x;
+*x = *y;
+*y = temp + (*x);
+printf("%d ",*y);
+}
+
